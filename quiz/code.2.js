e413132e0376cda59a8f8584dc276b00ee446a2a
omarMostafa73 <75231592+omarMostafa73@users.noreply.github.com>
2022-08-25 17:38:05 +0200

    Delete quiz directory

--- quiz/code.2.js
+++
@@ -1,9 +0,0 @@
-//problem 2
-setTimeout( function number1(){
-    console.log('hello')},1500)
-
-setTimeout( function number2(){
-    console.log('this is my js script')},1000)
-
-setTimeout(function number3(){
-    console.log('my name is andria')},500)
